d033c87c90dd908e598241485380c58f6f53cd45
jan.buettiker <jan.buettiker@hftm.ch>
2022-05-13 14:06:44 +0200

    Added entry model class

---
+++ src/main/java/ch/hftm/Model/Entry.java
@@ -0,0 +1,64 @@
+package ch.hftm.Model;
+
+import java.util.Date;
+
+public class Entry {
+
+    private String title;
+    private String description;
+    private String content;
+    private Date creationDate;
+    private Date lastEditDate;
+
+    public Entry() {
+        this.creationDate = new Date();
+    }
+
+    public Entry(String title, String content) {
+        this();
+        this.title = title;
+        this.content = content;
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getLastEditDate() {
+        return lastEditDate;
+    }
+
+    public void setLastEditDate(Date lastEditDate) {
+        this.lastEditDate = lastEditDate;
+    }
+
+}
