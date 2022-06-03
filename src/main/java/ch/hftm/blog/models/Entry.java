cf4b2aaeba0ccd7c83b20a9d9d879d72021687a3
jan.buettiker <jan.buettiker@hftm.ch>
2022-06-03 14:14:35 +0200

    added explicit id to have a workaround for the startup errrors

--- src/main/java/ch/hftm/blog/models/Entry.java
+++ src/main/java/ch/hftm/blog/models/Entry.java
@@ -3,11 +3,18 @@ package ch.hftm.blog.models;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
 @Entity
-public class Entry extends PanacheEntity {
+public class Entry extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
 
     public String title;
     public String description;
